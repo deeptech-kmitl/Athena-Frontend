5747da670067eb61ce715efa4425bef517867d84
Rerksurat <64070094@kmitl.ac.th>
2024-03-28 23:01:35 +0700

    add ui

--- src/pages/credit/index.tsx
+++ src/pages/credit/index.tsx
@@ -1,16 +1,22 @@
+import * as React from 'react';
 import { useEffect, useState } from 'react';
 
+// ** MUI Import
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Grid from '@mui/material/Grid'
-
+import FormControlLabel from '@mui/material/FormControlLabel';
 import CustomTextField from 'src/@core/components/mui/text-field';
+import { CardContent } from '@mui/material'
+import Checkbox from '@mui/material/Checkbox';
+
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 import CustomAvatar from 'src/@core/components/mui/avatar'
+import CustomChip from 'src/@core/components/mui/chip'
 
 import { ProjectTableRowType } from 'src/@fake-db/types';
 
@@ -19,6 +25,9 @@ import OptionsMenu from 'src/@core/components/option-menu';
 import { FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import { hexToRGBA } from 'src/@core/utils/hex-to-rgba';
+
+// ** Types
+import { ThemeColor } from 'src/@core/layouts/types'
 
 interface CellType {
     row: ProjectTableRowType
@@ -122,6 +131,39 @@ const Credits = () => {
             }
         },
     ]
+    interface DataType {
+        percent: string
+        credit: string
+        amountinst: string
+        hours: string
+        status: 'verified' | 'discount' | 'pending' | 'on-hold'
+    }
+
+    type StatusObj = Record<
+        DataType['status'],
+        {
+            text: string
+            color: ThemeColor
+        }
+    >
+
+    const creedit: DataType[] = [
+        {
+            credit: '470',
+            status: 'discount',
+            amountinst: '20',
+            hours: '88',
+            percent: '-6%'
+        }
+    ]
+
+    const statusObj: StatusObj = {
+        discount: { text: 'discount', color: 'error' },
+        pending: { text: 'Pending', color: 'secondary' },
+        'on-hold': { text: 'On hold', color: 'warning' },
+        verified: { text: 'Verified', color: 'success' }
+    }
+
 
     // ** State
     // const [data] = useState(props)
@@ -139,6 +181,12 @@ const Credits = () => {
     const handleFilter = (val: string) => {
         setValue(val)
     }
+
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
 
     return (
         <Grid >
@@ -193,6 +241,152 @@ const Credits = () => {
                     </div>
                 </Grid>
             </Grid>
+            <Grid container spacing={6} sx={{ mt: 2 }}>
+                <Grid item xs={12} md={6}>
+                    <Card>
+                        <CardHeader
+                            title='Credits Tracker'
+                            subheader='This Semester'
+                            sx={{
+                                color: '#FFFFFF99',
+                                fontSize: '18px',
+                                fontWeight: 600
+                            }}
+                            action={
+                                <OptionsMenu
+                                    options={['Show all entries', 'Refresh', 'Download']}
+                                    iconButtonProps={{ size: 'small', sx: { color: 'text.disabled' } }}
+                                />
+                            }
+                        />
+
+                        <CardContent>
+                            {creedit.map((row: DataType) => {
+                                return (
+                                    <Box sx={{ width: '100%' }}>
+                                        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                            <Typography sx={{ color: '#FFFFFF99', fontSize: '38px', fontWeight: 600, mr: 2 }}>
+                                                {row.credit}
+                                            </Typography>
+                                            <CustomChip
+                                                rounded
+                                                size='small'
+                                                skin='light'
+                                                label={row.percent}
+                                                color={statusObj[row.status].color}
+                                            />
+                                        </Box>
+                                        <Typography sx={{ color: '#FFFFFF66', fontSize: '15px', fontWeight: 400 }}>
+                                            Credits Balance
+                                        </Typography>
+
+                                        <Box sx={{ display: 'flex', alignItems: 'center', mr: 2, mt: 5 }}>
+                                            <CustomAvatar skin='light' variant='rounded' sx={{ mr: 2, width: '34px', height: '34px' }}>
+                                                <Icon icon='tabler:server' fontSize='1.2rem' />
+                                            </CustomAvatar>
+
+                                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                                <Typography sx={{ color: '#FFFFFF99', fontSize: '15px', fontWeight: 600 }}>
+                                                    Instance Created
+                                                </Typography>
+                                                <Typography sx={{ color: '#8692D0', fontSize: '13px', fontWeight: 400 }}>
+                                                    {row.amountinst}
+                                                </Typography>
+                                            </Box>
+                                        </Box>
+
+                                        <Box sx={{ display: 'flex', alignItems: 'center', mr: 2, mt: 5 }}>
+                                            <CustomAvatar skin='light' variant='rounded' sx={{ mr: 2, width: '34px', height: '34px' }}>
+                                                <Icon icon='tabler:clock' fontSize='1.2rem' style={{ color: '#00CFE8' }} />
+                                            </CustomAvatar>
+
+                                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                                <Typography sx={{ color: '#FFFFFF99', fontSize: '15px', fontWeight: 600 }}>
+                                                    Reserved Hours
+                                                </Typography>
+                                                <Typography sx={{ color: '#8692D0', fontSize: '13px', fontWeight: 400 }}>
+                                                    {row.hours}
+                                                </Typography>
+                                            </Box>
+                                        </Box>
+
+                                    </Box>
+                                )
+                            })}
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                    <Card>
+                        <CardHeader
+                            title='Credit Transfer'
+                            sx={{
+                                color: '#FFFFFF99',
+                                fontSize: '18px',
+                                fontWeight: 600
+                            }}
+                        />
+
+                        <CardContent sx={{ mt: -4 }}>
+                            <Typography sx={{ color: '#FFFFFF99', fontWeight: 400, fontSize: '14px' }}>
+                                Please provide the recipient's email or username and specify the amount of credits you wish to transfer.
+                            </Typography>
+
+                            <Box sx={{ mt: 10 }}>
+                                <form>
+                                    <Grid container spacing={5}>
+                                        <Grid item xs={6}>
+                                            <CustomTextField
+                                                fullWidth
+                                                label='Username / Email'
+                                                placeholder='63070094@it.kmitl.ac.th'
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <CustomTextField
+                                                fullWidth
+                                                label='Amount'
+                                                placeholder='120'
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Box>
+
+                            <Grid container sm={12} md={12} sx={{ mt: 5 }}>
+                                <Grid item sm={10} md={10}>
+                                    <FormControlLabel
+                                        label="I understand that transferring the credit(s) is irreversible."
+                                        control={<Checkbox onChange={handleChange} />}
+                                    />
+                                </Grid>
+                                <Grid item sm={2} md={2}>
+                                    <Button
+                                        type='submit'
+                                        variant='contained'
+                                        disabled={!checked}
+                                        sx={{
+                                            py: '10px',
+                                            px: '20px',
+                                            mr: 3,
+                                            backgroundColor: '#7367F0',
+                                            color: '#ffff',
+                                            '&:hover': {
+                                                backgroundColor: '#7367F0',
+                                                color: '#FFFFFF'
+                                            }
+                                        }}
+                                    >
+                                        Transfer <Icon fontSize='1.125rem' icon='tabler:send' />
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+
             < Card sx={{ mt: '30px' }}>
                 <CardHeader
                     action={
